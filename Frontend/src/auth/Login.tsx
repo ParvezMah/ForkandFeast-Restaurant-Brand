09f38f3090948d22a9271e495f6aa6fb01a8961f
Parvez Mahamud AA <parvezmahmudaa100@gmail.com>
2024-10-13 00:29:48 +0600

    Login page Completed

--- Frontend/src/auth/Login.tsx
+++ Frontend/src/auth/Login.tsx
@@ -2,35 +2,55 @@ import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Separator } from "@/components/ui/separator";
 import { Loader2, Lock, Mail } from "lucide-react"
+import { ChangeEvent, FormEvent, useState } from "react";
 import { Link } from "react-router-dom";
 
+interface loginInputState {
+    email: string,
+    password: string,
+}
+
 const Login = () => {
+    const [input, setInput] = useState<loginInputState>({
+        email:"", 
+        password: "",
+    });
+
+    const changeEventHandler = (e:ChangeEvent<HTMLInputElement>) => {
+        const {name, value} = e.target;
+        setInput({...input, [name]:value});
+    }
+
+    const loginSubmitHandler = (e:FormEvent) =>{
+        e.preventDefault();
+        console.log(input)
+    }
     const loading = false;
   return (
     <div className="flex items-center justify-center min-h-screen">
-        <form className="md:p-8 w-full max-w-md rounded-lg md:border border-gray-200">
+        <form onSubmit={loginSubmitHandler} className="md:p-8 w-full max-w-md rounded-lg md:border border-gray-200">
             <div>
                 <h1 className="font-bold text-2xl">Fork&Feast</h1>
             </div>
 
             <div className="mb-4">
                 <div className="relative">
-                    <Input type="email" placeholder="Email" className="pl-10 focus-visible:ring-1" />
+                    <Input value={input.email} onChange={changeEventHandler} name="email" type="email" placeholder="Email" className="pl-10 focus-visible:ring-1" />
                     <Mail className="absolute inset-y-2 left-3 text-gray-500 pointer-events-none"/>
                 </div>
             </div>
                 
             <div className="mb-4">
                <div className="relative">
-                    <Input type="password" placeholder="Password" className="pl-10 focus-visible:ring-1" />
+                    <Input value={input.password} onChange={changeEventHandler} name="password" type="password" placeholder="Password" className="pl-10 focus-visible:ring-1" />
                     <Lock className="absolute inset-y-2 left-3 text-gray-500 pointer-events-none"/>
                 </div>
             </div>
 
             <div className="mb-10">
                 {
-                    loading ? <Button className="w-full bg-orange hover:bg-hoverOrange"><Loader2 className="mr-2 w-4 animate-spin"/>Please Wait</Button>
-                            : <Button className="w-full bg-orange hover:bg-hoverOrange">Login</Button>
+                    loading ? <Button disabled className="w-full bg-orange hover:bg-hoverOrange"><Loader2 className="mr-2 w-4 animate-spin"/>Please Wait</Button>
+                            : <Button type="submit" className="w-full bg-orange hover:bg-hoverOrange">Login</Button>
                 }
                 
             </div>
