ab131b02f910cf20457b937cd50624b11ea0d714
Parvez Mahamud AA <parvezmahmudaa100@gmail.com>
2024-10-12 12:28:00 +0600

    Frontend Setup

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -5,7 +5,7 @@ import { Button } from './components/ui/button'
 function App() {
   return (
     <>
-        <Button>Let's Build it food app</Button>
+        <Button className='bg-orange hover:bg-hoverOrange'>Let's Build it food app</Button>
     </>
   )
 }
