209f79fef57f81509bf786cb2206bd4d2ba4e09a
Parvez Mahamud AA <parvezmahmudaa100@gmail.com>
2024-11-12 00:23:09 +0600

    Routes for Users completed

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./db/connectDB.js"
+import userRoute from "../Backend/routes/user.routes.js";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
 
 
@@ -8,6 +12,20 @@ dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+
+// Default Middleware for MERN project
+app.use(bodyParser.json({limit:'10mb'}));
+app.use(express.urlencoded({extended:true, limit:'10mb'}));
+app.use(express.json());
+app.use(cookieParser())
+const corsOptions = {
+    origin: "http://localhost:5173/",
+    credentials: true,
+}
+app.use(cors(corsOptions))
+
+//api route
+app.use("/api/v1/user", userRoute);
 
 app.listen(PORT, ()=>{
     connectDB();
